0c9f9fb9920ec9706c7d07e5dabf6eca4860cba7
stersk <sters@ua.fm>
2020-04-21 20:42:02 +0300

    Make multiple spring security configuration (REST/WEB)

--- src/main/java/org/qtee/dashboard/configuration/SecurityConfigWeb.java
+++ src/main/java/org/qtee/dashboard/configuration/SecurityConfigWeb.java
@@ -18,67 +18,87 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 @EnableWebSecurity
 @Order(1)
-public class SecurityConfigWeb extends WebSecurityConfigurerAdapter {
+public class SecurityConfigWeb {
     @Autowired
     private UserDetailsService userDetailsService;
 
     @Autowired
     private UserRepository repository;
 
-    @Override
-    public void init(WebSecurity web) throws Exception {
-        initAdminAccount();
-
-        super.init(web);
+    @Configuration
+    @Order(1)
+    public static class RestConfigurationAdapter extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            //@formatter:off
+            http.antMatcher("/services/**")
+                    .authorizeRequests().anyRequest().hasRole("ADMINISTRATOR")
+                    .and().httpBasic()
+                    .and()
+                    .csrf()
+                    .ignoringAntMatchers("/h2-console/**", "/services/**")
+                    .and().exceptionHandling().accessDeniedPage("/");
+            //@formatter:on
+        }
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .antMatchers("/home", "/orders", "/")
+    @Configuration
+    @Order(2)
+    public class App1ConfigurationAdapter extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                    .authorizeRequests()
+                    .antMatchers("/home", "/orders", "/")
                     .access("hasAnyRole('ROLE_USER', 'ROLE_ADMINISTRATOR')")
-                .antMatchers("/register")
+                    .antMatchers("/register")
                     .access("hasRole('ROLE_ADMINISTRATOR')")
-                .antMatchers("/**")
+                    .antMatchers("/**")
                     .access("permitAll")
 
-                .and()
-                .formLogin()
-                .loginPage("/login")
+                    .and()
+                    .formLogin()
+                    .loginPage("/login")
 
-                .and()
-                .logout()
-                .logoutSuccessUrl("/")
+                    .and()
+                    .logout()
+                    .logoutSuccessUrl("/")
 
-                // Make H2-Console non-secured; for debug purposes
-                //TODO make REST authorization and remove them from ignoringAntMachers
-                // Also temporary left REST-services unsecured
-                .and()
-                .csrf()
-                .ignoringAntMatchers("/h2-console/**", "/services/**")
+                    // Make H2-Console non-secured; for debug purposes
+                    .and()
+                    .csrf()
+                    .ignoringAntMatchers("/h2-console/**")
 
-                // Allow pages to be loaded in frames from the same origin; needed for H2-Console
-                .and()
-                .headers()
-                .frameOptions()
-                .sameOrigin()
-        ;
+                    // Allow pages to be loaded in frames from the same origin; needed for H2-Console
+                    .and()
+                    .headers()
+                    .frameOptions()
+                    .sameOrigin()
+            ;
+
+        }
+
+        @Override
+        protected void configure(AuthenticationManagerBuilder auth)
+                throws Exception {
+
+            auth
+                    .userDetailsService(userDetailsService)
+                    .passwordEncoder(encoder());
+
+        }
+
+        @Override
+        public void init(WebSecurity web) throws Exception {
+            initAdminAccount();
+
+            super.init(web);
+        }
     }
 
     @Bean
     public PasswordEncoder encoder() {
         return new BCryptPasswordEncoder(5);
-    }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth)
-            throws Exception {
-
-        auth
-                .userDetailsService(userDetailsService)
-                .passwordEncoder(encoder());
-
     }
 
     private void initAdminAccount() {
