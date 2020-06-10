0edc133394a297ff8ff2cbfff1bd3130cc45627f
stersk <sters@ua.fm>
2020-06-10 12:15:23 +0300

    Make auto redirect from http to https and add some changes for Spring context initiation in test class

--- src/test/java/ua/com/tracktor/DashboardApplicationTests.java
+++ src/test/java/ua/com/tracktor/DashboardApplicationTests.java
@@ -1,13 +1,33 @@
 package ua.com.tracktor;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.mock.env.MockEnvironment;
+import org.springframework.test.context.ContextConfiguration;
+import ua.com.tracktor.configuration.WebConfig;
 
 @SpringBootTest
-class DashboardApplicationTests {
+@ContextConfiguration(classes = WebConfig.class,
+		initializers = DashboardApplicationTests.class)
+public class DashboardApplicationTests implements ApplicationContextInitializer<ConfigurableApplicationContext>{
+	@Autowired
+	ApplicationContext context;
 
 	@Test
 	void contextLoads() {
+
 	}
 
+	@Override
+	public void initialize(ConfigurableApplicationContext applicationContext) {
+		MockEnvironment environment = new MockEnvironment();
+		environment.setProperty("server.ssl-redirection-port", 	"8080");
+		environment.setProperty("server.port", 					"8443");
+
+		applicationContext.setEnvironment(environment);
+	}
 }
