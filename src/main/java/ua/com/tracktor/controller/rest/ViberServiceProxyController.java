c9e9c657601ae3cb55971bc1172047b5f5989601
stersk <sters@ua.fm>
2020-07-22 00:06:46 +0300

    Process proxied error result as 200 with error payload in body, and return it to client. Add limit to inbound status-check queries as 1 query per n seconds.

--- src/main/java/ua/com/tracktor/controller/rest/ViberServiceProxyController.java
+++ src/main/java/ua/com/tracktor/controller/rest/ViberServiceProxyController.java
@@ -1,12 +1,19 @@
 package ua.com.tracktor.controller.rest;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import ua.com.tracktor.entity.Account;
 import ua.com.tracktor.entity.User;
@@ -16,6 +23,9 @@ import javax.servlet.http.HttpServletRequest;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.security.Principal;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 
@@ -28,11 +38,14 @@ public class ViberServiceProxyController {
     @Autowired
     private Environment env;
 
+    private final Map<Account, LocalDateTime> lastAccountQueryTime = new HashMap<>();
+
     @RequestMapping(value = "/**")
-    @ResponseBody
-    public String viberMirror(Principal principal, @RequestBody String body, @RequestHeader Map<String, String> headers, HttpMethod method, HttpServletRequest request) throws URISyntaxException
+    public ResponseEntity<String> viberMirror(Principal principal, @RequestBody String body, @RequestHeader MultiValueMap<String, String> headers, HttpMethod method, HttpServletRequest request) throws URISyntaxException
     {
+        int secondsPerQuerylimit = Integer.parseInt(Objects.requireNonNull(env.getProperty("viber-service.server.status-query-limit-time-in-seconds")));
         String server = env.getProperty("viber-service.server.address");
+        String basePath = env.getProperty("viber-service.server.path");
         int port = Integer.parseInt(Objects.requireNonNull(env.getProperty("viber-service.server.port")));
 
         UsernamePasswordAuthenticationToken authenticationToken = (UsernamePasswordAuthenticationToken) principal;
@@ -41,18 +54,50 @@ public class ViberServiceProxyController {
 
         Account account = user.getAccount();
         if (account == null) {
-            return "{\"error\":\"Account not authorized\"}";
+            return new ResponseEntity<>("{\"error\":\"Account not authorized\"}", HttpStatus.UNAUTHORIZED);
         }
 
-        URI uri = new URI("https", null, server, port, request.getRequestURI(), request.getQueryString(), null);
+        String path = basePath + request.getRequestURI().substring(9);
 
-        HttpEntity<String> httpEntity = new HttpEntity<>(body);
-        httpEntity.getHeaders().add("1c-user", account.getId().toString());
+        // If it is status check query, use limit no more than 1 query per 10 seconds. Another queries we process without limit
+        if (path.endsWith("get-status")) {
+            LocalDateTime currentTime = LocalDateTime.now();
+            LocalDateTime lastTime = lastAccountQueryTime.getOrDefault(account, LocalDateTime.now().plusSeconds(-3600));
+            if (ChronoUnit.SECONDS.between(lastTime, currentTime) > secondsPerQuerylimit) {
+                lastAccountQueryTime.put(account, currentTime);
+            } else {
+                return new ResponseEntity<>("", HttpStatus.TOO_MANY_REQUESTS);
+            }
+        }
 
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<String> responseEntity =
-                restTemplate.exchange(uri, method, httpEntity, String.class);
+        URI uri = new URI("https", null, server, port, path, request.getQueryString(), null);
 
-        return responseEntity.getBody();
+        headers.add("x-user-id", account.getId().toString());
+        headers.remove("authorization"); // Authorization on viberService is disabled by default
+
+        HttpEntity<String> httpEntity = new HttpEntity<>(body, headers);
+        ResponseEntity<String> responseEntity;
+
+        try {
+            RestTemplate restTemplate = new RestTemplate();
+            responseEntity = restTemplate.exchange(uri, method, httpEntity, String.class);
+
+        } catch (Exception e) {
+            Map<String, String> data = new HashMap<>();
+            data.put("error", "Query send error");
+            data.put("reason", e.getLocalizedMessage());
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            String stringData = "";
+            try {
+                stringData = objectMapper.writeValueAsString(data);
+            } catch (JsonProcessingException jsonProcessingException) {
+                stringData = e.getLocalizedMessage();
+            }
+
+            responseEntity = new ResponseEntity<>(stringData, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        return responseEntity;
     }
 }
